a4455215a7425365ed55ff16d855a2e52150df79
kmindubaev <kmindubaev@vizex.ru>
2024-06-16 17:35:04 -0700

    style: commented unused types

--- lib/types/store.ts
+++ lib/types/store.ts
@@ -1,5 +1,5 @@
 import type { CacheMap } from './cache'
-import type { ExtractQuery, QueryConfig, QueryInit, QueryStore } from './query-config'
+import type { QueryInit, QueryStore } from './query-config'
 import type { QueryResponse } from './query-response'
 import type { AsyncFunction } from './utils'
 
@@ -13,28 +13,28 @@ export interface QueryCache<C extends QueryInit> {
 	) => C['suspense'] extends true ? Awaited<ReturnType<A>> : QueryResponse<A>
 }
 
-type QueryAction<F extends AsyncFunction> = (...args: Parameters<F>) => QueryResponse<F>
+// type QueryAction<F extends AsyncFunction> = (...args: Parameters<F>) => QueryResponse<F>
 
-type SuspensedQueryAction<F extends AsyncFunction> = (
-	...args: Parameters<F>
-) => Awaited<ReturnType<F>>
+// type SuspensedQueryAction<F extends AsyncFunction> = (
+// 	...args: Parameters<F>
+// ) => Awaited<ReturnType<F>>
 
-export type QueryActions<T extends QueryStore> = {
-	[key in keyof T['queries']]: T['suspense'] extends true
-		? T['queries'][key] extends QueryConfig
-			? T['queries'][key]['suspense'] extends false
-				? QueryAction<ExtractQuery<T['queries'][key]>>
-				: SuspensedQueryAction<ExtractQuery<T['queries'][key]>>
-			: SuspensedQueryAction<ExtractQuery<T['queries'][key]>>
-		: T['queries'][key] extends QueryConfig
-			? T['queries'][key]['suspense'] extends true
-				? SuspensedQueryAction<ExtractQuery<T['queries'][key]>>
-				: QueryAction<ExtractQuery<T['queries'][key]>>
-			: QueryAction<ExtractQuery<T['queries'][key]>>
-}
+// export type QueryActions<T extends QueryStore> = {
+// 	[key in keyof T['queries']]: T['suspense'] extends true
+// 		? T['queries'][key] extends QueryConfig
+// 			? T['queries'][key]['suspense'] extends false
+// 				? QueryAction<ExtractQuery<T['queries'][key]>>
+// 				: SuspensedQueryAction<ExtractQuery<T['queries'][key]>>
+// 			: SuspensedQueryAction<ExtractQuery<T['queries'][key]>>
+// 		: T['queries'][key] extends QueryConfig
+// 			? T['queries'][key]['suspense'] extends true
+// 				? SuspensedQueryAction<ExtractQuery<T['queries'][key]>>
+// 				: QueryAction<ExtractQuery<T['queries'][key]>>
+// 			: QueryAction<ExtractQuery<T['queries'][key]>>
+// }
 
-export type MutationActions<T extends QueryStore> = {
-	[key in keyof T['mutations']]: T['mutations'][key]
-}
+// export type MutationActions<T extends QueryStore> = {
+// 	[key in keyof T['mutations']]: T['mutations'][key]
+// }
 
 export type ZustandQueries<S extends QueryStore> = QueryCache<S>
